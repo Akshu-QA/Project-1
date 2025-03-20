ca55c99ad8ad75863ab25d66f7f6101ef2071f5e
Akshu-QA <akshukalia26@gmail.com>
2025-03-20 19:21:07 +0530

    akshu comitt 20 march

--- src/test/java/base/BaseTest.java
+++
@@ -1,8 +0,0 @@
-package base;
-
-public class BaseTest {
-	public void m1() {
-		System.out.println("akshu");
-	}
-
-}
--- src/test/java/testCases/RadioButton.java
+++ src/test/java/testCases/RadioButton.java
@@ -5,5 +5,9 @@ public class RadioButton {
 	public void m3() {
 		System.out.println("git hub commit from Ankit");
 	}
+	public void m4() {
+		System.out.println("git hub commit from Akshu");
+	}
+
 
 }
