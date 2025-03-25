fe1d51e1880b8c20cb85e5a217773506b379357e
Akshu-QA <akshukalia26@gmail.com>
2025-03-25 10:16:13 +0530

    Ankit commit 25 mar 1f

--- src/test/java/resources/StringWordsCount.java
+++ src/test/java/resources/StringWordsCount.java
@@ -6,10 +6,10 @@ public class StringWordsCount {
 
 	public static void main(String[] args) {
 		String s = "Ankit and Akshu are highest paid QA with 25 lakhs of package and good QA and Ankit and also Akshu QA";
-		String[] a = s.split(" ");
+		char[] a = s.toCharArray();
 
-		HashMap<String, Integer> hm = new HashMap<String, Integer>();
-		for (String ref : a) {
+		HashMap<Character, Integer> hm = new HashMap<Character, Integer>();
+		for (char ref : a) {
 			if (hm.containsKey(ref)) {
 				int x = hm.get(ref);
 				hm.put(ref, x + 1);
@@ -18,8 +18,8 @@ public class StringWordsCount {
 			}
 		}
 
-		for (String ref1 : hm.keySet()) {
-			System.out.println("Repeated word is: + " + ref1 + " = " + hm.get(ref1));
+		for (char ref1 : hm.keySet()) {
+			System.out.println("Repeated word is " + ref1 + " = " + hm.get(ref1));
 		}
 
 	}
