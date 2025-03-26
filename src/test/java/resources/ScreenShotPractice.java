a4c6aeb86f5749b7f9e8d5e928d77a5290a75e5a
Akshu-QA <Akshukalia26@gmail.com>
2025-03-26 13:28:42 +0530

    26 march commit 1b

--- src/test/java/resources/ScreenShotPractice.java
+++ src/test/java/resources/ScreenShotPractice.java
@@ -2,9 +2,11 @@ package resources;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -18,12 +20,18 @@ public class ScreenShotPractice {
 	public static void main(String[] args) throws IOException {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		WebElement linksLoc = driver.findElement(By.cssSelector("div[id='gf-BIG']"));
+		driver.manage().window().maximize();
+		WebElement linksLoc = driver.findElement(By.xpath(("//div[@id='gf-BIG']")));
+
+		// scrollCode
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView();", linksLoc);
 
 		// screenshot code
 		File f = ((TakesScreenshot) linksLoc).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(f, new File("add location of your file here with name of file as ss1.png"));
+		FileUtils.copyFile(f, new File("C:\\Users\\Ankit\\Desktop\\AUTOMATION DOCS\\screenshots\\ss.png"));
 		driver.close();
 
 	}
