8c48d294d250d8196fa188df615c1cd80eccbded
Akshu-QA <Akshukalia26@gmail.com>
2025-03-31 15:51:08 +0530

    31March ankit commit 2a

--- src/test/java/resources/ReplaceStringProg.java
+++ src/test/java/resources/ReplaceStringProg.java
@@ -7,19 +7,22 @@ public class ReplaceStringProg {
 	public static void main(String[] args) {
 
 		String s = "Automation testing QA Ankit";
+		String s1 = "start";
 		char a[] = s.toCharArray();
 
-		HashMap<Character, Character> hm = new HashMap<>();
-		hm.put('a', '2');
-		hm.put('A', '2');
+//		HashMap<Character, Character> hm = new HashMap<>();
+//		hm.put('a', '2');
+//		hm.put('A', '2');
 
 		StringBuilder sb = new StringBuilder();
+	//	System.out.println(sb.append(s1).append("LT").append(" Akshu"));
+		System.out.println(sb.append(s1).insert(2, "X"));
 
-		for (char ref : a) {
-			sb.append(hm.getOrDefault(ref, ref));
-		}
-		
-		System.out.println(sb.toString());
+//		for (char ref : a) {
+//			sb.append(hm.getOrDefault(ref, ref));
+//		}
+//
+//		System.out.println(sb.toString());
 	}
 
 }
